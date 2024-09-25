112cebc6a78bad2ee323cb4795b3b7e514f3293d
Javier E Montes <jaxloz2002@gmail.com>
2024-09-25 14:41:03 -0500

    first commit

---
+++ src/layout/appLayout.vue
@@ -0,0 +1,11 @@
+<template>
+    <div id="app-layout">
+        <navBar />
+        <main>
+
+        </main>
+    </div>
+</template>
+<script setup>
+import navBar from '@/components/navBar.vue'
+</script>
