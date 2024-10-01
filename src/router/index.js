2d1235ceb0bff0dd6d6764dad91a823ce9562c5a
Miguel-A-Negrete <154093586+Miguel-A-Negrete@users.noreply.github.com>
2024-10-01 13:52:23 -0500

    Vista tareas

--- src/router/index.js
+++ src/router/index.js
@@ -30,6 +30,11 @@ const router = createRouter({
             path: '/sesion',
             name: 'sesioh',
             component: () => import('@/views/sessions_actas.vue')
+          },
+          {
+            path: '/tareas',
+            name: 'tareas',
+            component: () => import('@/views/tareas_actas.vue')
           }
       ]
     },
