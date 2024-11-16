9e72732648d695b6d961d5094fbc73148ed059f0
moises5silpe <moises5silpe@gmail.com>
2024-11-15 20:05:40 -0500

    barra de busqueda para solicitantes agregada

--- src/views/solicitantes/SolicitantesView.vue
+++ src/views/solicitantes/SolicitantesView.vue
@@ -30,7 +30,7 @@
                                             d="M2 12C2 6.477 6.477 2 12 2s10 4.477 10 10-4.477 10-10 10S2 17.523 2 12Zm11-4.243a1 1 0 1 0-2 0V11H7.757a1 1 0 1 0 0 2H11v3.243a1 1 0 1 0 2 0V13h3.243a1 1 0 1 0 0-2H13V7.757Z"
                                             clip-rule="evenodd" />
                                     </svg>
-                                    <span>Crear acta</span>
+                                    <span>Registrar solicitante</span>
                                 </button>
                             </li>
                         </ul>
@@ -45,7 +45,7 @@
                                 d="m19 19-4-4m0-7A7 7 0 1 1 1 8a7 7 0 0 1 14 0Z" />
                         </svg>
                     </div>
-                    <input type="text" id="table-search-users"
+                    <input v-model="nombreSolicitante" @input="searchSolicitante" type="text" id="table-search-users"
                         class="block p-2 ps-10 text-sm text-gray-900 border border-gray-300 rounded-lg w-80 bg-gray-50 focus:ring-blue-500 focus:border-blue-500 dark:bg-gray-700 dark:border-gray-600 dark:placeholder-gray-400 dark:text-white dark:focus:ring-blue-500 dark:focus:border-blue-500"
                         placeholder="Buscar solicitante">
                 </div>
@@ -150,8 +150,9 @@
 <script setup>
 import useSolicitanteStore from '@/stores/solicitante';
 import SolicitanteModal from './SolicitanteModal.vue';
+import debounce from '@/utils/debounce';
 import { initFlowbite } from 'flowbite';
-import { useTemplateRef, onMounted } from 'vue';
+import { useTemplateRef, ref, onMounted } from 'vue';
 import { storeToRefs } from 'pinia';
 
 
@@ -167,6 +168,14 @@ const {
     fetchSolicitantes,
     deleteSolicitante
 } = solicitanteStore;
+
+
+const nombreSolicitante = ref('');
+
+const searchSolicitante = debounce(async () => {
+    if(!nombreSolicitante.value) return;
+    await fetchSolicitantes(`filter[nombre]=${nombreSolicitante.value}`);
+}, 500);
 
 const openUpdateSolicitanteModal = (solicitante) => {
     solicitanteModal.value.setSolicitante(solicitante)
